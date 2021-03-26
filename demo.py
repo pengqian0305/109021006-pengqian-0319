383664218217740cc7789cfcd9f33a57d27b4b07
pengqian0305 <109021006@live.asia.edu.tw>
2021-03-26 15:49:51 +0800

    aa

--- demo.py
+++ demo.py
@@ -1,3 +1,19 @@
-import requests 
-req = requests.get("http://210.70.80.21/~bs109021006/")
-print(req.status_code)
+import requests
+from bs4 import BeautifulSoup
+
+req = requests.get("http://dns2.asia.edu.tw/~jdwang/PaperList.htm")
+req.encoding="uft8"
+if req.status_code==200 :
+    #print(req.text)
+    Soup = BeautifulSoup(req.text,"lxml")
+    #print(Soup)
+    result1 = Soup.find_all("li")
+    fp=open("out3.txt","w",encoding="utf8")
+    for val in result1:
+        text2=val.text.replace('\n','')
+        text3=text2.replace('  ','')
+        print(text3)
+        fp.write(text3 +"\n")
+    fp.close()
+else:
+    print("no page")
